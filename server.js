245519494dd5a7009ae3807b21e58b2751f28936
Maxime <maxime.gordon@gmail.com>
2021-02-05 23:19:18 -0500

    done ?

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express()
 const port = process.env.PORT || 8000
 app.use(express.static(__dirname + "/"))
 
+let dripToggleState = 0;
+let arduinoState = 0;
+let dripRate = 10;
+
 // const options = {
 //   hostname: 'youmakemecry.com',
 //   key: fs.readFileSync('/etc/letsencrypt/live/youmakemecry.com/privkey.pem'),
@@ -19,23 +23,38 @@ const server = http.createServer(app)
 server.listen(port)
 console.log("http server listening on %d", port)
 
-const wss = new WebSocketServer({ server: server })
+const wss = new WebSocketServer({
+  server: server
+})
 console.log("websocket server created")
 
 // get websocket connection
-wss.on("connection", function (ws) {
-  let id = setInterval(function () {
-    console.log("send ping: C")
-    ws.send("PING", function () {})
+wss.on("connection", function(ws) {
+  let id = setInterval(function() {
+    ws.send("PING", function() {})
   }, 12000)
   console.log("websocket connection open ")
-
-  ws.on("message", function (message) {
+  ws.send(`T,${dripToggleState}`);
+  ws.send(`R,${dripRate}`);
+  if (arduinoState == 1) {
+    ws.send('XX');
+  }
+  ws.on("message", function(message) {
     console.log("received: %s", message)
     wss.broadcast(message)
+    if (message == 'XX') {
+      arduinoState = 1;
+    } else if (message == 'T,0') {
+      dripToggleState = 0;
+    } else if (message == 'T,1') {
+      dripToggleState = 1;
+    } else if (message.startsWith("R")) {
+      let value = message.split(',')[1]
+      dripRate = value;
+    }
   })
 
-  ws.on("close", function () {
+  ws.on("close", function() {
     console.log("websocket connection close")
     clearInterval(id)
   })
--- test.js
+++ test.js
@@ -7,7 +7,9 @@ const port = new SerialPort('/dev/tty.usbmodem14101', { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));// Read the port data
 
 const WebSocket = require('ws');
-
+let maxValue = 21;
+let min = 1;
+let max = 20; 
 
 //const ws = new WebSocket('wss://youmakemecry.com:8000/');
 const ws = new WebSocket('ws://localhost:8000/');
@@ -25,9 +27,11 @@ ws.on('message', function incoming(data) {
 
     console.log(data);
     if (data.startsWith('R')){
-    	let val =data.split(",")[1];
+    	let val = maxValue-(data.split(",")[1]);
+      val = Math.min(Math.max(val, min), max);
     	console.log(val);
-    	port.write(`${val}\n`);
+    	port.write((`${val}\n`));
+ 
     }
     else if (data.startsWith('T')){
       port.write(`F`);
@@ -39,6 +43,23 @@ ws.on('message', function incoming(data) {
 port.on("open", () => {
   console.log('serial port open');
 });
+
 parser.on('data', data =>{
   console.log('got word from arduino:', data);
+  //console.log(typeof data);
+  // console.log(JSON.stringify(data));
+  let val = data.replace( /[\r\n]+/gm, "" );
+  // console.log(data);
+  if(val === "XX"){
+     ws.send(val);    
+  }
 });
+
+port.on("close", () => {
+  ws.send("XY");
+});
+
+port.on("error", (err) => {
+  ws.send("XY");
+});
+
